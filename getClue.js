7e57f80e2601d8a6e84450d9a9b815c62885df31
Kevin Mei <kevjinx@gmail.com>
2021-05-13 06:31:35 -0700

    fetch with value and catId

--- getClue.js
+++ getClue.js
@@ -1,6 +1,12 @@
+const fetch = require('node-fetch')
 
+const api = 'https://jservice.io/api/clues'
 
-const getClue = () => {
+// `/?value=${valueInput}&category=${catIdInput}`
 
-  const result = fetch('')
+const getClueValueCat = async (valueInput, catIdInput) => {
+  const result = await fetch(`${api}/?value=${valueInput}&category=${catIdInput}`)
+  const json = await result.json()
+  console.log(json);
+  return json;
 }
