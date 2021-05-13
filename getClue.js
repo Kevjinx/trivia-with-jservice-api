b31a93138b3c5470d858e058b876390a2855a015
Kevin Mei <kevjinx@gmail.com>
2021-05-13 06:52:18 -0700

    json handler for question, answer, etc

--- getClue.js
+++ getClue.js
@@ -10,3 +10,18 @@ const getClueValueCat = async (valueInput, catIdInput) => {
   console.log(json);
   return json;
 }
+
+
+const jsonHandler = async fetchJson => {
+  let obj = {}
+  const json = await fetchJson
+  console.log(json);
+  obj.question = json[0].question
+  obj.answer = json[0].answer
+  obj.clueCount = json[0].category.clues_count
+  obj.invalidCount = json[0].invalid_count
+  console.log(obj);
+  return obj
+}
+
+jsonHandler(getClueValueCat(100, 1))
